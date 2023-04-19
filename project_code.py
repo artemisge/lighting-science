ee15f3cc99fcf1a108f2c688b11f2dc48180928d
Artemis <artemis.georgopoulou@gmail.com>
2023-04-19 17:08:11 +0200

    debugging task2

--- project_code.py
+++ project_code.py
@@ -1,5 +1,5 @@
 from DMXEnttecPro import Controller
-from luxpy.toolboxes.spectro import jeti
+from luxpy.toolboxes import spectro as sp# import jeti
 import luxpy as lx
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,7 +18,8 @@ def changeColors(*args):
     dmx.submit()
 
 # doesn't work. path needs to be replaced
-dmx = Controller("/dev/ttyUSB0")  # Typical of Linux
+#dmx = Controller("/dev/ttyUSB0")  # Typical of Linux
+dmx = Controller("COM7")
 # try sudo -i, ls /dev before after, udevadm monitor
 # and plug in device and then sudo chmod a+rw /dev/ttyUSB0
 
@@ -29,18 +30,20 @@ dmx = Controller("/dev/ttyUSB0")  # Typical of Linux
 sp.init('jeti')
 
 # global var
-step = 15
-
+step = 85#15
+wavelengths_array = []
+wl_saved = False
 # TASK 2
 # Measure spectra of all channels (maybe not UV)
 def auto_measure(UV_on):
     array_measured_spectra = [] # in the end it will be [5 or 6]x[255/step]
-
+    global wavelengths_array
     if UV_on:
         end = 6 # UV on
     else:
-        end = 5 # UV off
-        
+        end = 2 # UV off
+    tmp1 = True
+    tmp2= True
     for i in range(end):
         channel_array = []
         # step = 15 -> 17 different measurements
@@ -54,8 +57,16 @@ def auto_measure(UV_on):
             
             changeColors(*DMX_arguments_tmp_array)
             #print(sp.__file__)
-            spd = jeti.get_spd() 
-            channel_array.append(spd)
+            spd = sp.get_spd(manufacturer='jeti')
+            if tmp1:
+                print("spectra size " + str(np.array(spd).shape)) 
+                tmp1 = False
+                wavelengths_array = spd[0]
+                #print(wavelengths_array)
+            channel_array.append(spd[1])
+        if tmp2:
+            print("channel array size: " + str(np.array(channel_array).shape))
+            tmp2 = False
         array_measured_spectra.append(channel_array)
 
     return array_measured_spectra
@@ -64,13 +75,23 @@ def make_luminance_plots(array_measured_spectra):
     # convert to photometric units using the CIE 1931 2° observer (default):
     array_luminances = []
     array_normalized_luminances = []
+    print("len: "+str(len(array_measured_spectra)))
+    # for every channel (=5)
     for i in range(len(array_measured_spectra)):
         array_luminance_i = []
+        # for every measurement of a driver value in this channel (=255/step)
         for j in range(len(array_measured_spectra[0])):
-            array_luminance_i.append(lx.spd_to_power(array_measured_spectra[i][j], ptype = 'pu'))
+            tmp = lx.spd_to_power(np.vstack([wavelengths_array, array_measured_spectra[i][j]]), ptype = 'pu')
+            array_luminance_i.append(tmp[0][0]) # for some reason spd_to_power returns a 3D array instead of a single value
+            # print("illuminance of channel measurement: " + str(np.array(tmp).shape))
         # now we have all luminances for all measurements for different driver values for this specific i-channel. And we'll make a plot
+
+        print(array_luminance_i)
+        print("size of illum: " +str(np.array(array_luminance_i).shape))
+
         x = [i for i in range(0, 255, step)]
-        plt.plot(x, array_luminance_i)
+        print(x)
+        plt.plot(x, array_luminance_i, **{'color': 'lightsteelblue', 'marker': 'o'})
         plt.xlabel('Driver Values')
         plt.ylabel('Luminance')
         plt.title('Luminance for different driver values')
@@ -78,8 +99,12 @@ def make_luminance_plots(array_measured_spectra):
         array_luminances.append(array_luminance_i)
 
         # also do the normalized luminance (to the max luminance of each channel)
-        array_normalized_luminance_i = lx.spd_normalize(array_luminance_i, norm_type = 'max')
+        array_normalized_luminance_i = lx.spd_normalize(np.array(array_luminance_i), norm_type = 'max', wl=False)
+        print(np.array(array_luminance_i).shape)
+
         array_normalized_luminances.append(array_normalized_luminance_i)
+        print("normalized" + str(array_normalized_luminance_i))
+        
         plt.plot(x, array_normalized_luminance_i)
         plt.xlabel('Driver Values')
         plt.ylabel('Normalized Luminance')
@@ -104,7 +129,14 @@ def interpolate_normalized_luminances(array_normalized_luminances):
 # call function to measure all channels, with UV off, and print result.
 # it should be a 5x17 array. (17, because we took intervals of 15 while testing different driver values for each channel: 0-255)
 
-array_measured_spectra = auto_measure(False)
+beautiful_thing = array_measured_spectra = auto_measure(False)
+#print(beautiful_thing)
+print(" SIZEEE :" + str(np.array(beautiful_thing).shape))
+n_l = make_luminance_plots(beautiful_thing)
+# x,y=interpolate_normalized_luminances(n_l)
+# print(x,y)
+
+
 
 
 # ____TASK 3____
